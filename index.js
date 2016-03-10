803110a9b2f8a2dab567230491f961ca413946f8
YuniorGlez <yunior.developer@hotmail.com>
2016-03-10 21:33:24 +0000

    Make it better

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ app.get('/', function (request, response) {
 app.get('/notices', function (req, res) {
     noticias.find(function (err, items) {
     if (err) throw err;
-        console.log(items);
-        res.send(items);
+        res.json(items);
     });
 });
 
