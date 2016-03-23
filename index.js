c4b62a70084aec7641860aa24dbf65bc426980a9
YuniorGlez <yunior.developer@hotmail.com>
2016-03-23 13:41:40 +0000

    make it better

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var noticias;
 var uri = 'mongodb://heroku_ng4vzrc8:7eiqqmqn0rldusdpvt2rb6u4hg@ds011419.mlab.com:11419/heroku_ng4vzrc8';
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -21,31 +20,28 @@ app.get('/', function (request, response) {
     response.render('index.html');
 });
 
-
-
-
 app.get('/notices', function (req, res) {
+    console.log(typeof parseInt(req.params.id));
     noticias.find(function (err, items) {
         if (err) throw err;
-        res.json(items);
+        console.log(items);
+        res.json(items); // LINEA QUE PETA
     });
 });
-
 app.delete('/notices', function (req, res) {
-    noticias.remove({},function (err, items) {
+    noticias.remove({}, function (err,items) {
         if (err) throw err;
-        console.log(JSON.parse(items));
+        console.log(items); 
         res.send('Todo borrado');
     });
-});
-
+}); 
 app.post('/notices', function (req, res) {
-    noticias.save(req.body,function (err, items) {
+    console.log(req.body);
+    noticias.insert(req.body, function (err, items) {
         if (err) throw err;
         res.send('Metido');
     });
 });
-
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
